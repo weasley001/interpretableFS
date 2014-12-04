bc28bbb969501268741d75c04c6a23ab7bceb24e
weasley <weasley001@gmail.com>
2014-12-04 14:23:52 +0800

    functuon update
    use matplotlib show fuzzy rules

--- src/frb.py
+++ src/frb.py
@@ -1,8 +1,12 @@
+import argparse, os, sys, traceback
 from sklearn import datasets, cluster, preprocessing
 from sklearn.metrics import confusion_matrix
+from scipy.stats import gaussian_kde
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 import numpy as np
 import pandas as pd
-import argparse, os, sys, traceback
+
 
 class FuzzyRuleBase:
 
@@ -83,7 +87,8 @@ if __name__ == '__main__':
     #read input training file and show some information
     print("input file: %s"% (os.path.basename(infile_path)))
     print("rules per class: %d"% (rulesperclass))
-    df=pd.read_csv(infile_path, sep=',')
+    df = pd.read_csv(infile_path, sep=',')
+    df_feature_name = df.drop("CLASS",axis=1).columns.values
     raw_data = df.drop("CLASS", axis=1).values
     data_target_names = df.CLASS.unique()
     print("number of class: %d"% (len(data_target_names)))
@@ -100,8 +105,8 @@ if __name__ == '__main__':
     #Scaling inupt data set
     scaler = preprocessing.StandardScaler().fit(raw_data)
     np.set_printoptions(precision=2,suppress=False,formatter={'float': '{: 0.3f}'.format})
-    #print(scaler.mean_)
-    #print(scaler.std_)
+    print(scaler.mean_)
+    print(scaler.std_)
     sys.stdout.flush()
     data_scaled = scaler.transform(raw_data)
     #np.savetxt("scaled.csv", data_scaled , delimiter=",")
@@ -110,7 +115,8 @@ if __name__ == '__main__':
     R1 = FuzzyRuleBase(data_scaled,data_target,len(data_target_names),rulesperclass)
     myans = map(R1.predict,data_scaled)
     print(R1.m)
-    
+    print(R1.s)
+    sys.stdout.flush()
     ################################################################################
     #output confusion matrix and accuracy
     cm = confusion_matrix(data_target, myans)
@@ -118,3 +124,27 @@ if __name__ == '__main__':
     print(cm)
     print("-"*80)
     print("Accuracy:%2.2f" % (np.trace(cm)/(cm.sum()+0.0)))
+    sys.stdout.flush()
+    ################################################################################
+    #x = np.linspace(scaler.mean_.min()-2*scaler.std_.max(),scaler.mean_.max()+2*scaler.std_.max(),1024)
+    x = np.linspace(-4,4,1024)
+    xs = np.linspace(scaler.mean_.min()-2*scaler.std_.max(),scaler.mean_.max()+2*scaler.std_.max(),128)
+    gs = plt.GridSpec(R1.nrpc+1, R1.nc)
+    plt.subplot(gs[0,:])
+    for k in xrange(R1.dim):
+        density = gaussian_kde(raw_data[:,k])
+        density.covariance_factor = lambda : .20
+        density._compute_covariance()
+        plt.plot(xs,density(xs),label=''+df_feature_name[k])
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,ncol=R1.dim, mode="expand", borderaxespad=0.)
+
+    for c in xrange(R1.nr):
+        plt.subplot(gs[c%R1.nrpc+1,c/R1.nrpc])
+        for i,j,k in zip(R1.m[c],R1.s[c],xrange(R1.dim)):
+        #for i,j,k in zip(R1.m[c]+scaler.mean_,R1.s[c]*scaler.std_,xrange(R1.dim)):
+            plt.plot(x,np.exp(-(x-i)**2/(j**2)),label=''+df_feature_name[k])
+        #plt.legend() 
+   
+    
+    
+    plt.show()
